70fc3b3478c548a771b7eac2829a00f216fa6d79
Siwon Jeon <siwonjeon234@gmail.com>
2022-11-02 20:48:08 +0900

    add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,15 +7,15 @@ import Button from 'react-bootstrap/Button'
 const Home = () => {
     return(
         <Wrapper>
-        <Header>예비집사 판별기</Header>
-        <Contents>
-        <Title>나에게 맞는 주인님은?</Title>
-        <LogoImage>
-            <img src = {PangImage} class="rounded-circle" width={350} height={350} />
-        </LogoImage>
-        <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-        <Button>테스트 시작하기</Button>
-        </Contents>
+            <Header>예비집사 판별기</Header>
+            <Contents>
+                <Title>나에게 맞는 주인님은?</Title>
+                <LogoImage>
+                    <img src = {PangImage} class="rounded-circle" width={350} height={350} />
+                </LogoImage>
+                <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
+                <Button>테스트 시작하기</Button>
+            </Contents>
         </Wrapper>
     )
 }
@@ -23,34 +23,34 @@ const Home = () => {
 export default Home;
 
 const Wrapper = styled.div`
-height: 100vh;
-width: 100%;
+    height: 100vh;
+    width: 100%;
 `
 
 const Header = styled.div`
-font-size: 40pt;
-display: flex;
-justify-content: center;
-align-items: center;
+    font-size: 40pt;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 const Contents = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `
 
 const Title = styled.div`
-font-size: 30pt;
-margin-top: 40px;
+    font-size: 30pt;
+    margin-top: 40px;
 `
 
 const LogoImage = styled.div`
-margin-top: 10px;
+    margin-top: 10px;
 `
 
 const Desc = styled.div`
-font-size: 20pt;
-margin-top: 20px;
+    font-size: 20pt;
+    margin-top: 20px;
 `
